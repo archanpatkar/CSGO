5d4dc39b07610dd05eabebf7617604bb1184da15
Archan Patkar <archanpatkar@gmail.com>
2018-05-16 10:49:18 +0530

    Client

---
+++ Client.py
@@ -0,0 +1,10 @@
+import socket;
+
+conn = socket.socket();
+
+conn.connect(("localhost",8081));
+
+data = "";
+while data != "q":
+  data = input("Enter a Message> ")
+  conn.send(data.encode(encoding='utf_8',errors='strict'))
